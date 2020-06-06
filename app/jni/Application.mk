ea3c05173e15170aa279224978218fd98176d0d2
R4SAS <r4sas@i2pmail.org>
2020-06-06 05:27:29 +0300

    add submodules, fix paths

--- app/jni/Application.mk
+++ app/jni/Application.mk
@@ -1,12 +1,4 @@
-#APP_ABI := armeabi-v7a x86
-#APP_PLATFORM := android-14
-
-# ABI arm64-v8a and x86_64 supported only from platform-21
-#APP_ABI := arm64-v8a x86_64
-#APP_PLATFORM := android-21
-
 NDK_TOOLCHAIN_VERSION := clang
-#APP_STL := c++_shared
 APP_STL := c++_static
 
 # Enable c++17 extensions in source code
@@ -17,19 +9,13 @@ ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 APP_CPPFLAGS += -DANDROID_ARM7A
 endif
 
-# git clone https://github.com/PurpleI2P/Boost-for-Android-Prebuilt.git -b boost-1_72_0
-# git clone https://github.com/PurpleI2P/OpenSSL-for-Android-Prebuilt.git
-# git clone https://github.com/PurpleI2P/MiniUPnP-for-Android-Prebuilt.git
-# git clone https://github.com/PurpleI2P/android-ifaddrs.git
-# change to your own
-I2PD_LIBS_PATH = /path/to/libraries
-BOOST_PATH = $(I2PD_LIBS_PATH)/Boost-for-Android-Prebuilt
-OPENSSL_PATH = $(I2PD_LIBS_PATH)/OpenSSL-for-Android-Prebuilt
-MINIUPNP_PATH = $(I2PD_LIBS_PATH)/MiniUPnP-for-Android-Prebuilt
-IFADDRS_PATH = $(I2PD_LIBS_PATH)/android-ifaddrs
+BOOST_PATH    = $(NDK_MODULE_PATH)/boost
+OPENSSL_PATH  = $(NDK_MODULE_PATH)/openssl
+MINIUPNP_PATH = $(NDK_MODULE_PATH)/miniupnp
+IFADDRS_PATH  = $(NDK_MODULE_PATH)/android-ifaddrs
 
 # don't change me
-I2PD_SRC_PATH = $(PWD)/..
+I2PD_SRC_PATH = $(NDK_MODULE_PATH)/i2pd
 
 LIB_SRC_PATH = $(I2PD_SRC_PATH)/libi2pd
 LIB_CLIENT_SRC_PATH = $(I2PD_SRC_PATH)/libi2pd_client
