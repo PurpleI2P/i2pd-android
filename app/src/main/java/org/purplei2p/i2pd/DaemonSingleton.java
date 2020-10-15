d4110e64d95044f952e046dc8c724791a7272d98
R4SAS <r4sas@i2pmail.org>
2020-10-15 04:31:35 +0300

    i2pd 2.33.0-63, tabulation, gradle configuration
    Signed-off-by: R4SAS <r4sas@i2pmail.org>

--- app/src/main/java/org/purplei2p/i2pd/DaemonSingleton.java
+++ app/src/main/java/org/purplei2p/i2pd/DaemonSingleton.java
@@ -2,180 +2,183 @@ package org.purplei2p.i2pd;
 
 import java.util.HashSet;
 import java.util.Set;
+
 import android.os.Environment;
 import android.util.Log;
 
 import org.purplei2p.i2pd.R;
 
 public class DaemonSingleton {
-	private static final String TAG = "i2pd";
-	private static final DaemonSingleton instance = new DaemonSingleton();
+    private static final String TAG = "i2pd";
+    private static final DaemonSingleton instance = new DaemonSingleton();
 
-	public interface StateUpdateListener {
-		void daemonStateUpdate();
-	}
+    public interface StateUpdateListener {
+        void daemonStateUpdate();
+    }
 
-	private final Set<StateUpdateListener> stateUpdateListeners = new HashSet<>();
+    private final Set<StateUpdateListener> stateUpdateListeners = new HashSet<>();
 
-	public static DaemonSingleton getInstance() {
-		return instance;
-	}
+    public static DaemonSingleton getInstance() {
+        return instance;
+    }
 
-	public synchronized void addStateChangeListener(StateUpdateListener listener) {
-		stateUpdateListeners.add(listener);
-	}
+    public synchronized void addStateChangeListener(StateUpdateListener listener) {
+        stateUpdateListeners.add(listener);
+    }
 
-	public synchronized void removeStateChangeListener(StateUpdateListener listener) {
-		stateUpdateListeners.remove(listener);
-	}
+    public synchronized void removeStateChangeListener(StateUpdateListener listener) {
+        stateUpdateListeners.remove(listener);
+    }
 
-	private synchronized void setState(State newState) {
-		if (newState == null)
-			throw new NullPointerException();
+    private synchronized void setState(State newState) {
+        if (newState == null)
+            throw new NullPointerException();
 
-		State oldState = state;
+        State oldState = state;
 
-		if (oldState == null)
-			throw new NullPointerException();
+        if (oldState == null)
+            throw new NullPointerException();
 
-		if (oldState.equals(newState))
-			return;
+        if (oldState.equals(newState))
+            return;
 
-		state = newState;
-		fireStateUpdate1();
-	}
+        state = newState;
+        fireStateUpdate1();
+    }
 
-	public synchronized void stopAcceptingTunnels() {
-		if (isStartedOkay()) {
-			setState(State.gracefulShutdownInProgress);
-			I2PD_JNI.stopAcceptingTunnels();
-		}
-	}
+    public synchronized void stopAcceptingTunnels() {
+        if (isStartedOkay()) {
+            setState(State.gracefulShutdownInProgress);
+            I2PD_JNI.stopAcceptingTunnels();
+        }
+    }
 
-	public synchronized void startAcceptingTunnels() {
-		if (isStartedOkay()) {
-			setState(State.startedOkay);
-			I2PD_JNI.startAcceptingTunnels();
-		}
-	}
+    public synchronized void startAcceptingTunnels() {
+        if (isStartedOkay()) {
+            setState(State.startedOkay);
+            I2PD_JNI.startAcceptingTunnels();
+        }
+    }
 
-	public synchronized void reloadTunnelsConfigs() {
-		if (isStartedOkay()) {
-			I2PD_JNI.reloadTunnelsConfigs();
-		}
-	}
+    public synchronized void reloadTunnelsConfigs() {
+        if (isStartedOkay()) {
+            I2PD_JNI.reloadTunnelsConfigs();
+        }
+    }
 
-	public synchronized int GetTransitTunnelsCount() {
-		return I2PD_JNI.GetTransitTunnelsCount();
-	}
+    public synchronized int GetTransitTunnelsCount() {
+        return I2PD_JNI.GetTransitTunnelsCount();
+    }
 
-	private volatile boolean startedOkay;
+    private volatile boolean startedOkay;
 
-	public enum State {
-		uninitialized(R.string.uninitialized),
-		starting(R.string.starting),
-		jniLibraryLoaded(R.string.jniLibraryLoaded),
-		startedOkay(R.string.startedOkay),
-		startFailed(R.string.startFailed),
-		gracefulShutdownInProgress(R.string.gracefulShutdownInProgress),
-		stopped(R.string.stopped);
+    public enum State {
+        uninitialized(R.string.uninitialized),
+        starting(R.string.starting),
+        jniLibraryLoaded(R.string.jniLibraryLoaded),
+        startedOkay(R.string.startedOkay),
+        startFailed(R.string.startFailed),
+        gracefulShutdownInProgress(R.string.gracefulShutdownInProgress),
+        stopped(R.string.stopped);
 
-		State(int statusStringResourceId) {
-			this.statusStringResourceId = statusStringResourceId;
-		}
+        State(int statusStringResourceId) {
+            this.statusStringResourceId = statusStringResourceId;
+        }
 
-		private final int statusStringResourceId;
+        private final int statusStringResourceId;
 
-		public int getStatusStringResourceId() {
-			return statusStringResourceId;
-		}
-	};
+        public int getStatusStringResourceId() {
+            return statusStringResourceId;
+        }
+    }
 
-	private volatile State state = State.uninitialized;
+    ;
 
-	public State getState() {
-		return state;
-	}
+    private volatile State state = State.uninitialized;
 
-	{
-		setState(State.starting);
-		new Thread(new Runnable() {
+    public State getState() {
+        return state;
+    }
 
-			@Override
-			public void run() {
-				try {
-					I2PD_JNI.loadLibraries();
-					setState(State.jniLibraryLoaded);
-				} catch (Throwable tr) {
-					lastThrowable = tr;
-					setState(State.startFailed);
-					return;
-				}
-				try {
-					synchronized (DaemonSingleton.this) {
-						I2PD_JNI.setDataDir(Environment.getExternalStorageDirectory().getAbsolutePath() + "/i2pd");
-						daemonStartResult = I2PD_JNI.startDaemon();
-						if ("ok".equals(daemonStartResult)) {
-							setState(State.startedOkay);
-							setStartedOkay(true);
-						} else
-							setState(State.startFailed);
-					}
-				} catch (Throwable tr) {
-					lastThrowable = tr;
-					setState(State.startFailed);
-				}
-			}
+    {
+        setState(State.starting);
+        new Thread(new Runnable() {
 
-		}, "i2pdDaemonStart").start();
-	}
+            @Override
+            public void run() {
+                try {
+                    I2PD_JNI.loadLibraries();
+                    setState(State.jniLibraryLoaded);
+                } catch (Throwable tr) {
+                    lastThrowable = tr;
+                    setState(State.startFailed);
+                    return;
+                }
+                try {
+                    synchronized (DaemonSingleton.this) {
+                        I2PD_JNI.setDataDir(Environment.getExternalStorageDirectory().getAbsolutePath() + "/i2pd");
+                        daemonStartResult = I2PD_JNI.startDaemon();
+                        if ("ok".equals(daemonStartResult)) {
+                            setState(State.startedOkay);
+                            setStartedOkay(true);
+                        } else
+                            setState(State.startFailed);
+                    }
+                } catch (Throwable tr) {
+                    lastThrowable = tr;
+                    setState(State.startFailed);
+                }
+            }
 
-	private Throwable lastThrowable;
-	private String daemonStartResult = "N/A";
+        }, "i2pdDaemonStart").start();
+    }
 
-	private void fireStateUpdate1() {
-		Log.i(TAG, "daemon state change: " + state);
-		for (StateUpdateListener listener : stateUpdateListeners) {
-			try {
-				listener.daemonStateUpdate();
-			} catch (Throwable tr) {
-				Log.e(TAG, "exception in listener ignored", tr);
-			}
-		}
-	}
+    private Throwable lastThrowable;
+    private String daemonStartResult = "N/A";
 
-	public Throwable getLastThrowable() {
-		return lastThrowable;
-	}
+    private void fireStateUpdate1() {
+        Log.i(TAG, "daemon state change: " + state);
+        for (StateUpdateListener listener : stateUpdateListeners) {
+            try {
+                listener.daemonStateUpdate();
+            } catch (Throwable tr) {
+                Log.e(TAG, "exception in listener ignored", tr);
+            }
+        }
+    }
 
-	public String getDaemonStartResult() {
-		return daemonStartResult;
-	}
+    public Throwable getLastThrowable() {
+        return lastThrowable;
+    }
 
-	private final Object startedOkayLock = new Object();
+    public String getDaemonStartResult() {
+        return daemonStartResult;
+    }
 
-	public boolean isStartedOkay() {
-		synchronized (startedOkayLock) {
-			return startedOkay;
-		}
-	}
+    private final Object startedOkayLock = new Object();
 
-	private void setStartedOkay(boolean startedOkay) {
-		synchronized (startedOkayLock) {
-			this.startedOkay = startedOkay;
-		}
-	}
+    public boolean isStartedOkay() {
+        synchronized (startedOkayLock) {
+            return startedOkay;
+        }
+    }
 
-	public synchronized void stopDaemon() {
-		if (isStartedOkay()) {
-			try {
-				I2PD_JNI.stopDaemon();
-			} catch(Throwable tr) {
-				Log.e(TAG, "", tr);
-			}
+    private void setStartedOkay(boolean startedOkay) {
+        synchronized (startedOkayLock) {
+            this.startedOkay = startedOkay;
+        }
+    }
 
-			setStartedOkay(false);
-			setState(State.stopped);
-		}
-	}
+    public synchronized void stopDaemon() {
+        if (isStartedOkay()) {
+            try {
+                I2PD_JNI.stopDaemon();
+            } catch (Throwable tr) {
+                Log.e(TAG, "", tr);
+            }
+
+            setStartedOkay(false);
+            setState(State.stopped);
+        }
+    }
 }
