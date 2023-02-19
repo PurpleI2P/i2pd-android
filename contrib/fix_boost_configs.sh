48875c11982a0e54e56d91c2484c1471a03f2484
startyourday60 <116428444+startyourday60@users.noreply.github.com>
2023-02-20 03:50:21 +0700

    fix_boost_configs

---
+++ contrib/fix_boost_configs.sh
@@ -0,0 +1,8 @@
+for file in configs/*1_78_0*
+do
+        echo "~~FILE~~"
+        echo $file
+        newname=```echo $file | sed "s/ndk23/ndk19/"```
+        echo "cp $file to $newname"
+        cp $file $newname
+done
